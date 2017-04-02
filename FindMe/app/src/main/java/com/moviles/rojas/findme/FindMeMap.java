6e013007855e4a0455d643036880887506fad708
Jose Rojas <JOSEJ94@users.noreply.github.com>
2017-04-02 00:25:11 -0600

    Se arregló un problema de permisos
    Se agregó lineas de solicitud de permisos.

--- FindMe/app/src/main/java/com/moviles/rojas/findme/FindMeMap.java
+++ FindMe/app/src/main/java/com/moviles/rojas/findme/FindMeMap.java
@@ -1,6 +1,8 @@
 package com.moviles.rojas.findme;
 
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -84,13 +86,7 @@ public class FindMeMap extends FragmentActivity implements OnMapReadyCallback {
 
     private void miUbicacion() { //detecta la ubicación actual del dispositivo.
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            Mensaje("No hay permiso");
             return;
         }
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
